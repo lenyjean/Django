a99beb07b48ab1b391190c9cbacfcbe9959b9f3d
Gian Carlo Llanes Garcia <49069702+SftwreDev@users.noreply.github.com>
2022-03-20 22:11:22 +0800

    Reviewed

--- orders/forms.py
+++ orders/forms.py
@@ -1,8 +1,28 @@
 from django import forms
 from .models import *
+import datetime
+from django.utils import timezone
+import pytz
+
+utc=pytz.UTC
+now = timezone.now()
+
+class DateInput(forms.DateInput):
+    input_type = 'date'
 
 class OrderForms(forms.ModelForm):
     class Meta: 
         model = Orders
-        fields = "__all__"
+        fields = [
+            "customer_name", "customer_address" , "products", "no_of_order", "total_amount", "pickup_date", "status"
+        ]
+        widgets = {
+            "pickup_date" : DateInput()
+        }
 
+        def clean(self):
+            cleaned_data = super().clean()
+            pickup_date = cleaned_data.get("pickup_date")
+
+            if pickup_date < datetime.date.today():
+                raise forms.ValidationError("Pick-up Date should not be later than date today")
--- orders/views.py
+++ orders/views.py
@@ -5,35 +5,53 @@ from orders.models import *
 from .forms import *
 
 # Create your views here.
+
+
 @login_required
 def orders_list(request):
     template_name = "orders/orders_list.html"
     orders = Orders.objects.all()
     context = {
-        "orders" : orders
+        "orders": orders
     }
-    return render (request, template_name, context)
+    return render(request, template_name, context)
+
 
 @login_required
 def orders_add(request):
     template_name = "orders/orders_add.html"
     form = OrderForms(request.POST or None)
     if form.is_valid():
-        form.save()
+        customer_name = form.cleaned_data['customer_name']
+        customer_address = form.cleaned_data['customer_address']
+        products = form.cleaned_data['products']
+        no_of_order = form.cleaned_data['no_of_order']
+        total_amount = form.cleaned_data['total_amount']
+        pickup_date = form.cleaned_data['pickup_date']
+        processed_by = request.user.username
+        status = form.cleaned_data['status']
+
+        orders = Orders.objects.get_or_create(
+            customer_name=customer_name, customer_address=customer_address, products=products,
+            no_of_order=no_of_order, total_amount=total_amount, pickup_date=pickup_date, processed_by=processed_by, status=status
+        )
+
         return redirect("orders-list")
     context = {
-       "form" :  form
+        "form":  form
     }
-    return render (request, template_name, context)
+    return render(request, template_name, context)
+
 
 @login_required
 def orders_view(request, pk):
-     template_name = "orders/orders_view.html"
-     orders = Orders.objects.filter(id=pk)
-     context = {
-         "orders" : orders
-     }
-     return render (request, template_name, context)
+    template_name = "orders/orders_view.html"
+    orders = Orders.objects.filter(id=pk)
+    context = {
+        "orders": orders
+    }
+    return render(request, template_name, context)
+
 
 @login_required
 def orders_update(request, pk):
@@ -44,12 +62,13 @@ def orders_update(request, pk):
         form.save()
         return redirect("orders-list")
     context = {
-       "form" :  form
+        "form":  form
     }
-    return render (request, template_name, context)
+    return render(request, template_name, context)
+
 
 @login_required
 def orders_delete(request, pk):
-     orders = Orders.objects.filter(id=pk)
-     orders.delete()
-     return redirect("orders-list")
+    orders = Orders.objects.filter(id=pk)
+    orders.delete()
+    return redirect("orders-list")
