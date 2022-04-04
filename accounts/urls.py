11b83e0b80459c6be5c86dc0f9af99e5bbc3b28d
Gian Carlo Llanes Garcia <49069702+SftwreDev@users.noreply.github.com>
2022-04-04 22:04:02 +0800

    reviewed

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,6 +1,8 @@
-# from django.urls import path
-# from .views import *
+from django.urls import path
+from .views import *
 
-# urlpatterns = [
-#     path("account-list", accounts, name="account-list")
-# ]
+urlpatterns = [
+    # path("account-list", accounts, name="account-list")
+    path("create-admin-account", AdminSignUpView.as_view(), name="admin-sign-up"),
+    path("create-staff-account", StaffSignUpView.as_view(), name="staff-sign-up"),
+]
