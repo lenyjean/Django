f7eddc2a135dfaab1a5ac74146c6e6d160234f08
Gian Carlo Llanes Garcia <49069702+SftwreDev@users.noreply.github.com>
2022-04-05 21:38:18 +0800

    Reviewed

--- bookings/models.py
+++ bookings/models.py
@@ -2,10 +2,11 @@ from django.db import models
 
 # Create your models here.
 class Bookings(models.Model):
+    messenger_id  = models.IntegerField()
     customer_name = models.CharField(max_length=225)
     product_ordered = models.CharField(max_length=225)
     category = models.CharField(max_length=225)
-    pickup_date = models.DateField(auto_now_add=False)
+    pickup_date = models.DateField(auto_now_add=True)
     status = models.CharField(max_length=225)
     created_date = models.DateField(auto_now_add=True)
 
