1abf3d32eb468e8169fbb93a64a4c9cc16c11727
Gian Carlo Llanes Garcia <49069702+SftwreDev@users.noreply.github.com>
2022-04-05 21:51:23 +0800

    Reviewed

--- webhooks/views.py
+++ webhooks/views.py
@@ -24,11 +24,11 @@ class Webhooks(viewsets.ViewSet):
     def create(self, request):
         if request.method == "POST":
             serializers = Webhooks(data=request.data)
-
+            print(serializers.data)
             messenger_id = serializers.data['entry'][0]['messaging'][0]["sender"]["id"]
             message = serializers.data['entry'][0]['messaging'][0]["message"]["text"]
             check_data = Bookings.objects.filter(messenger_id=messenger_id)
-            print(check_data)
+            
             if not check_data:
                 Bookings.objects.create(
                     messenger_id = serializers.data['entry'][0]['messaging'][0]["sender"]["id"]
