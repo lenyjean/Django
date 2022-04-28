782db9e1a7d112d5fe5d0db9858c2fe3ae115a3b
Ivy Marie <99119558+magpantayivymarie@users.noreply.github.com>
2022-04-28 21:53:35 +0800

    bugs fixes

--- products/models.py
+++ products/models.py
@@ -13,6 +13,10 @@ class Category(models.Model):
         return self.category
 
 class Products(models.Model):
+    status = (
+        ("Available" , "Available"),
+        ("Not Available" , "Not Available"),
+    )
     product_name = models.CharField(max_length=255)
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
     price = models.DecimalField(max_digits=10, decimal_places=2)
