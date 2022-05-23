7648e8a5de4d5165998950fcefb15fe67140b7bc
Gian Carlo Llanes Garcia <49069702+SftwreDev@users.noreply.github.com>
2022-05-24 05:05:09 +0800

    reviewed

--- webhooks/views.py
+++ webhooks/views.py
@@ -25,12 +25,17 @@ class Webhooks(viewsets.ViewSet):
     def create(self, request):
         if request.method == "POST":
             print(request.data)
-            MessengerData.objects.create(data=request.data)
-            serializers = InquriesSerializer(data=request.data)
-            
-            if serializers.is_valid():
-                serializers.save()
-                return Response(serializers.data, status=status.HTTP_201_CREATED)
-            else:
-                return Response(serializers.errors, status=status.HTTP_201_CREATED)
-            
+
+            if request.data['type'] == "custom_yes":
+                MessengerData.objects.create(data=request.data)
+                serializers = InquriesSerializer(data=request.data)
+                
+                if serializers.is_valid():
+                    serializers.save()
+                    return Response(serializers.data, status=status.HTTP_201_CREATED)
+                else:
+                    return Response(serializers.error, status=status.HTTP_201_CREATED)
+
+            elif request.data["type"] == "custom_no":
+                MessengerData.objects.create(data=request.data)
+                return Response(request.data, status=status.HTTP_201_CREATED)
