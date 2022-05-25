cbccb7c4567906dff531dc560ed6c333fe547d8d
Gian Carlo Llanes Garcia <49069702+SftwreDev@users.noreply.github.com>
2022-05-25 11:02:40 +0800

    reviewed

--- stsfbm/settings.py
+++ stsfbm/settings.py
@@ -71,15 +71,14 @@ CORS_ORIGIN_WHITELIST = (
 )
 
 MIDDLEWARE = [
-    "corsheaders.middleware.CorsMiddleware",
-    "django.middleware.common.CommonMiddleware",
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
-    # 'django.middleware.csrf.CsrfViewMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'stsfbm.urls'
