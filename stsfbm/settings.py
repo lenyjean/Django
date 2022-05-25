82f4d74cdf8f9b628c7fe3deb96f66de52b08a5c
Gian Carlo Llanes Garcia <49069702+SftwreDev@users.noreply.github.com>
2022-05-25 09:52:04 +0800

    reviewed

--- stsfbm/settings.py
+++ stsfbm/settings.py
@@ -157,3 +157,7 @@ LOGIN_URL = 'accounts/login'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = "accounts.User"
+
+CSRF_COOKIE_SECURE = True
+
+SECURE_BROWSER_XSS_FILTER = True
