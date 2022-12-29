015e2be198c6dd30c1e837f7e196fd0cf1b059ab
lenyjean <99113542+lenyjean@users.noreply.github.com>
2022-12-30 00:46:41 +0800

    size label on product  form

--- products/forms.py
+++ products/forms.py
@@ -4,7 +4,7 @@ from .models import *
 
 class ProductForms(forms.ModelForm):
     category = forms.ModelChoiceField(queryset=Category.objects.filter(status=True))
-    size = forms.CharField(max_length=255, label="Size : (in centimeters)")
+    size = forms.CharField(max_length=255, label="Cake size : (diameter x height in inches):")
     class Meta: 
         model = Products
         fields = ["product_name", "category", "price", "size", "status"]
